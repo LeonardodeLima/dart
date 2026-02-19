52a17154d08119813bb9ee4ef76b1bc7528afd0c
Leonardo Lima <leonardo.flatpb@gmail.com>
2026-02-19 18:32:24 -0300

    feat: implement var, const, and final declarations

---
+++ variaveis-tipos-de-dados/var_const_final.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  var curso = 'Dart';
+
+  final escola = stdin.readLineSync();
+
+  const double pi = 3.14;
+
+  print('Curso: $curso, Escola: $escola, Valor digitado: $pi');
+}
