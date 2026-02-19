b5247ae6121260c91817c50bdbe26290686cadea
Leonardo Lima <leonardo.flatpb@gmail.com>
2026-02-19 18:29:04 -0300

    feat: add numeric conversion script

---
+++ variaveis-tipos-de-dados/conversao_tipo_numerico.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int moeda = 10;
+  double salario = 1500.00;
+
+  // runtimeType shows the type of the variable
+  // double.parse() converts a string to a double
+  // int.parse() converts a string to an int
+  // moeda.toString(); converts a number to a string
+
+  salario.toString();
+
+  print(salario.toInt());
+  print(double.parse("10.0"));
+}
