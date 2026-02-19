90b22db2f461c4c3dabd17308dea37da25705739
Leonardo Lima <leonardo.flatpb@gmail.com>
2026-02-19 18:25:27 -0300

    feat: implement arithmetic operators

---
+++ operadores/operadores_aritimetico.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  print('soma: ${10 + 20}');
+  print('subtração: ${20 - 10}');
+  print('multiplicação: ${5 * 3}');
+  print('resto da divisão: ${23 % 3}');
+  print('divisão: ${23 / 3}');
+  print('divisão inteira: ${23 ~/ 3}');
+}
